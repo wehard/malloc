9d1104967a20bff2b6491627c83908daef11325f
Willehard Korander <willehard@gmail.com>
2021-03-30 12:16:23 +0300

    Separate test subject test 5

---
+++ test/test_subject_5.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_subject_5.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wkorande <willehard@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/23 16:43:01 by wkorande          #+#    #+#             */
+/*   Updated: 2021/03/30 11:56:19 by wkorande         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "unity.h"
+#include "malloc.h"
+#include "malloc_internal.h"
+#include "libft.h"
+#include <unistd.h>
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test5(void)
+{
+	ft_malloc(1024);
+	ft_malloc(1024 * 32);
+	ft_malloc(1024 * 1024);
+	ft_malloc(1024 * 1024 * 16);
+	ft_malloc(1024 * 1024 * 128);
+	show_alloc_mem();
+}
+
+int main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(test5);
+	return UNITY_END();
+}
